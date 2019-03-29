62b59415f92533f299e343782da8bd084484aaa4
Devin Jacobs <dbuttonz@gmail.com>
2019-03-29 10:55:36 -0600

    Setup warnings and scripts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#![warn(
+    warnings,
+    future_incompatible,
+    nonstandard_style,
+    rust_2018_compatibility,
+    rust_2018_idioms,
+    rustdoc,
+    unused
+)]
+
 fn main() {
     println!("Hello, world!");
 }
