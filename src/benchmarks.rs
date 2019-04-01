4e1be3d25629779794aba891d5e896589702c38b
Devin Jacobs <dbuttonz@gmail.com>
2019-04-01 14:38:50 -0600

    gen_image benchmark

---
+++ src/benchmarks.rs
@@ -0,0 +1,13 @@
+use criterion::{criterion_group, criterion_main, Bencher, Criterion};
+
+fn bench_gen_image(c: &mut Criterion) {
+    let image_sizes: Vec<i32> = vec![100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
+    c.bench_function_over_inputs(
+        "gen_image",
+        |b: &mut Bencher, size: &i32| b.iter(|| raytrace::gen_image(*size, size / 2)),
+        image_sizes,
+    );
+}
+
+criterion_group!(benches, bench_gen_image);
+criterion_main!(benches);
