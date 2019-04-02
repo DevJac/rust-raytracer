fc67e684b349d16bfe36472082ac21c5282324c6
Devin Jacobs <dbuttonz@gmail.com>
2019-04-02 11:24:29 -0600

    Benchmarks use Camera

--- src/benchmarks.rs
+++ src/benchmarks.rs
@@ -1,10 +1,18 @@
 use criterion::{criterion_group, criterion_main, Bencher, Criterion};
+use raytrace::vec3::Vec3;
+use raytrace::{gen_image, Camera, ORIGIN};
 
-fn bench_gen_image(c: &mut Criterion) {
-    let image_sizes: Vec<i32> = vec![100, 200, 400];
-    c.bench_function_over_inputs(
+fn bench_gen_image(criterion: &mut Criterion) {
+    let c = Camera {
+        origin: ORIGIN,
+        lower_left_corner: Vec3(-2.0, -1.0, -1.0),
+        horizontal: Vec3(4.0, 0.0, 0.0),
+        vertical: Vec3(0.0, 2.0, 0.0),
+    };
+    let image_sizes: Vec<f64> = vec![25.0, 50.0, 100.0];
+    criterion.bench_function_over_inputs(
         "gen_image",
-        |b: &mut Bencher, size: &i32| b.iter(|| raytrace::gen_image(*size, size / 2)),
+        move |b: &mut Bencher, size: &f64| b.iter(|| gen_image(c, *size)),
         image_sizes,
     );
 }
